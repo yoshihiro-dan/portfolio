d76b86f0c7166c49e1c469caf2b103931241eaf7
yoshihiro-dan <aerialact@gmail.com>
2024-01-17 21:49:27 +0900

    環境変数のテスト

--- components/posts.js
+++ components/posts.js
@@ -7,8 +7,10 @@ import { eyecatchLocal } from 'lib/constants'
 import ConvertBody from '@/components/convert-body'
 
 export const client = createClient({
-  serviceDomain: process.env.NEXT_PUBLIC_SERVICE_DOMAIN,
-  apiKey: process.env.NEXT_PUBLIC_API_KEY,
+  // serviceDomain: process.env.NEXT_PUBLIC_SERVICE_DOMAIN,
+  // apiKey: process.env.NEXT_PUBLIC_API_KEY,
+  serviceDomain: "satsuki-blog",
+  apiKey: "dc83ca7826cb4837a2a6259338081f31aa78",
 })
 
 export default async function Posts({ catID, limit = 10 }) {
