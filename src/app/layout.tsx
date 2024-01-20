f11f5fdd6a303c5faa529f9650762cdb5ca97308
yoshihiro-dan <aerialact@gmail.com>
2024-01-20 14:48:45 +0900

    noindexの設定

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,6 +21,19 @@ const montserrat = Montserrat({
 export const metadata = {
   title: 'Yoshihiro Dan Portfolio',
   description: '檀義弘のポートフォリオです',
+  robots: {
+    index: false,
+    follow: true,
+    nocache: true,
+    googleBot: {
+      index: true,
+      follow: false,
+      noimageindex: true,
+      'max-video-preview': -1,
+      'max-image-preview': 'large',
+      'max-snippet': -1,
+    },
+  },
 }
 
 export default function RootLayout({
